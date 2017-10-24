dde1dcf9e942d105e1003ce375d4fbbebbe7692c
root <root@localhost.localdomain>
2017-10-23 21:46:12 -0400

    added install script

--- .venv/lib/python3.5/site-packages/libnmap/objects/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from libnmap.objects.report import NmapReport
-from libnmap.objects.host import NmapHost
-from libnmap.objects.service import NmapService
-
-__all__ = ['NmapReport', 'NmapHost', 'NmapService']
